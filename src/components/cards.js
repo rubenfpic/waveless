6e2eb34af90658c73922dace9b567eca94c3ecd3
Rubén Fernández <rubenfernandez@MacBook-Pro-de-Ruben.local>
2025-06-27 18:42:41 +0200

    Cards layout

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,17 +1,46 @@
+function createCard(image) {
+  return `
+      <article class="card">
+        <div class="card__image">
+          <img src="/src/assets/img/${image}" alt="Destino destacado" />
+          <span class="card__tag">Quads</span>
+        </div>
+        <div class="card__body">
+          <p class="card__location"><strong>Marruecos, África</strong> 9 días</p>
+          <h3 class="card__title">Descubre Bangkok con Iberojet</h3>
+        </div>
+        <div class="card__footer">
+          <div class="card__price">
+            <strong>
+              <small>Desde</small>
+              248,00 €
+            </strong>
+            <button href="#" class="button button--link p-0">
+              Ver desglose<svg class="button__icon-16" aria-hidden="true">
+                <use xlink:href="/src/assets/svg/sprite.svg#chevron"></use>
+              </svg>
+            </button>
+          </div>
+          <button type="button" class="button button--sm button--secondary">Reservar</button>
+        </div>
+      </article>
+  `;
+}
+
 export function createCards() {
   const fragment = document.createRange().createContextualFragment(`
     <div class="cards__layout">
       <div class="cards__list">
         <h2 class="cards__title">Asia</h2>
-        <div class="cards__item">CARDS 1</div>
-        <div class="cards__item">CARDS 2</div>
-        <div class="cards__item">CARDS 3</div>
+        <div class="cards__item">${createCard("card1.png")}</div>
+        <div class="cards__item">${createCard("card2.png")}</div>
+        <div class="cards__item">${createCard("card3.png")}</div>
       </div>
       <div class="cards__list">
         <h2 class="cards__title">América</h2>
-        <div class="cards__item">CARDS 4</div>
-        <div class="cards__item">CARDS 5</div>
-        <div class="cards__item">CARDS 6</div>
+        <div class="cards__item">${createCard("card1.png")}</div>
+        <div class="cards__item">${createCard("card2.png")}</div>
+        <div class="cards__item">${createCard("card3.png")}</div>
       </div>
     </div>
   `);
